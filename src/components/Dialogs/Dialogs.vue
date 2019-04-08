624029cceadf0b8db33b01d885341c82ab0cf604
Christoph Hoffmann <christoph.hoffmann@oeaw.ac.at>
2019-04-08 15:23:33 +0200

    unneeded dialog removed

--- src/components/Dialogs/Dialogs.vue
+++ src/components/Dialogs/Dialogs.vue
@@ -2,20 +2,17 @@
   <div>
       <logindialog></logindialog>
       <logoutdialog></logoutdialog>
-      <createdialog></createdialog>
   </div>
 </template>
 
 <script>
 import logindialog from './LoginDialog';
 import logoutdialog from './LogoutDialog';
-import createdialog from './CreateDialog';
 
 export default {
   components: {
     logindialog,
     logoutdialog,
-    createdialog,
   },
 };
 </script>
