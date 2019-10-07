af40612f99dde63f24eb8b043b5133970d8d5a74
Christoph Hoffmann <christoph.hoffmann@oeaw.ac.at>
2019-10-07 12:16:21 +0200

    itemcount in listview extended

--- src/components/genericList/list.vue
+++ src/components/genericList/list.vue
@@ -89,7 +89,7 @@ export default {
           rec: null,
         },
         loading: false,
-        itemOptions: [10, 20, 50],
+        itemOptions: [10, 20, 50, 100, 250],
         totalHits: 0,
         pagination: {},
         q: {},
