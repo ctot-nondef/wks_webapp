19425481a22c19d2bdd1bd6d3c53e03893105617
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2023-01-18 09:13:17 +0100

    lint tuning

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
     extends: [
         // add more generic rulesets here, such as:
         // 'eslint:recommended',
-        'plugin:vue/base',
+        "plugin:vue/recommended",
         // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
     ],
     rules: {
         // override/add rules settings here, such as:
         // 'vue/no-unused-vars': 'error'
+        'vue/multi-word-component-names': 'off',
+        'vue/no-v-text-v-html-on-component': 'off'
     }
 }
